adb624261ede0a10bebfde23b7dff08e1e90d263
huyuanixn325 <huyuanxin0325@163.com>
2020-07-13 20:13:39 +0800

    创建类person接测试类personTest及编写测试方法

---
+++ src/test/java/com/thoughtworks/basic/PersonTest.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.basic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+    @Test
+    public void should_return_String_when_given_tom_and_21() {
+        //given
+        Person person = new Person('tom',21);
+        //when
+        String introduce = person.introduce();
+
+        //then
+        assertEquals(introduce," My name is Tom. I am 21 years old.");
+    }
+
+}
