09b1e4e0d55f2fd495ff8e82ed6561fbc3c4c970
huyuanixn325 <huyuanxin0325@163.com>
2020-07-13 20:55:41 +0800

    完成测试方法should_return_1_when_given_class”

--- src/main/java/com/thoughtworks/basic/KClass.java
+++ src/main/java/com/thoughtworks/basic/KClass.java
@@ -1,4 +1,49 @@
 package com.thoughtworks.basic;
 
+import java.util.List;
+
 public class KClass {
+    private int classNumber;
+    private String className;
+    private List<Student> students;
+    private Teacher teacher;
+
+    public KClass(int classNumber, String className, List<Student> students, Teacher teacher) {
+        this.classNumber = classNumber;
+        this.className = className;
+        this.students = students;
+        this.teacher = teacher;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
