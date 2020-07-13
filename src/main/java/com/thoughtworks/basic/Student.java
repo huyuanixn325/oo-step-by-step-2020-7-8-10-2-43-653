1fa21dbfbd48333099afb5f48982a605dafaf882
huyuanixn325 <huyuanxin0325@163.com>
2020-07-13 20:29:46 +0800

    创建student类的构造方法及重新父类方法

--- src/main/java/com/thoughtworks/basic/Student.java
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -1,7 +1,18 @@
 package com.thoughtworks.basic;
 
 public class Student extends Person{
+    private int classNumber;
     public Student(String name, int age) {
         super(name, age);
+    }
+
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce();
     }
 }
