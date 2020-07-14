35ea27c637a46e453bd483eb4562e6c7e85b7437
huyuanixn325 <huyuanxin0325@163.com>
2020-07-14 20:37:37 +0800

    完成方法should_return_3_when_given_studentList

--- src/test/java/com/thoughtworks/basic/ClassTest.java
+++ src/test/java/com/thoughtworks/basic/ClassTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.basic;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -25,13 +26,20 @@ public class ClassTest {
     public void should_return_3_when_given_studentList() {
         //given
         List<Student> students = new ArrayList<>();
+        Student student1 = new Student("张山",23);
+        Student student2 = new Student("李四",20);
+        Student student3 = new Student("王五",18);
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
         Teacher teacher = new Teacher("Matt",30,"Teacher");
         KClass myclass = new KClass(2,"二年级二班",students,teacher);
+        student1.setkClass(myclass);
+        student2.setkClass(myclass);
+        student3.setkClass(myclass);
         //when
-        Teacher teacherResult = myclass.getTeacher();
+       List<Student> studentsResult = myclass.getStudents();
         //then
-        assertEquals(teacherResult.getJob(),teacher.getJob());
-        assertEquals(teacherResult.getAge(),teacher.getAge());
-        assertEquals(teacherResult.getName(),teacher.getName());
+        Assert.assertEquals(studentsResult.size(),3);
     }
 }
