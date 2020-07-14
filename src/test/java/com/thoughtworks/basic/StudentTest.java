aaf9bdd6af2b2606f7b4d321346175ee360d11fc
huyuanixn325 <huyuanxin0325@163.com>
2020-07-14 21:17:00 +0800

    完成测试方法should_return_a_message_when_given_Tom_and_22_class_2_and_checkd_jack”

--- src/test/java/com/thoughtworks/basic/StudentTest.java
+++ src/test/java/com/thoughtworks/basic/StudentTest.java
@@ -61,4 +61,25 @@ public class StudentTest {
         Assert.assertEquals(messages.size(),1);
         assertEquals(messages.get(0),"My name is Jack. I am 21 years old. I am a Student of Class 2.");
     }
+
+    @Test
+    public void should_return_a_message_when_given_Tom_and_22_class_2_and_checkd_jack() {
+        //given
+        Student student = new Student("Tom",22);
+        Student student2 = new Student("wangna",23);
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        students.add(student2);
+        Teacher teacher = new Teacher("Matt",30,"Teacher");
+        KClass kClass = new KClass(2,"2班",students,teacher);
+        student.setkClass(kClass);
+        student2.setkClass(kClass);
+        student.changeName("Jack");
+        //when
+        List<String> messages = student2.getMessage();
+
+        //then
+        Assert.assertEquals(messages.size(),1);
+        assertEquals(messages.get(0),"My name is Jack. I am 22 years old. I am a Student of Class 2.");
+    }
 }
